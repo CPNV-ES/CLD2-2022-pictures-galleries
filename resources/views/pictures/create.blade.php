6428fe38447ea936e78d28a9dbfc28ba81ba7c32
Pascal Hurni <phi@pragmaticpurist.org>
2022-03-26 20:18:17 +0100

    Change to upload the picture directly to S3 by hooking the form in JS (put that JS code back in its usual location)

--- resources/views/pictures/create.blade.php
+++ resources/views/pictures/create.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="{{ route('galleries.pictures.store', $gallery) }}" enctype="multipart/form-data" method="POST">
+<form class="s3upload" action="{{ route('galleries.pictures.store', $gallery) }}" enctype="multipart/form-data" method="POST"
+ data-s3attributes="{{ json_encode($postObject->getFormAttributes()) }}"
+ data-s3inputs="{{ json_encode($postObject->getFormInputs()) }}"
+>
     @csrf
     <label for="title">Title:</label><br>
     <input type="text" id="title" name="title"><br>
+    <input type="hidden" name="path" value="{{ $path }}">
     <label  for="picture_file">
         picture
       </label>
@@ -12,4 +16,5 @@
       <input id="picture_file" name="picture_file" type="file"><br>
     <input type="submit" value="Ajout picture">
 </form> 
+<script src="{{ asset('js/s3upload.js') }}"></script>
 @endsection
